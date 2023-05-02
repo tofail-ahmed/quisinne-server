62dcd2ee6907fd7ec7fa5e7ee616a586f4c1147b
Tofail Ahmed Sayem <atofail50@gmail.com>
2023-05-02 17:08:33 +0600

    chefs data added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express=require('express')
 const app=express()
 const port=5000
-
+const chefs=require("./data/chef.json")
 app.get('/',(req,res)=>{
       res.send("welcome to Quisinne De French")
 })
+app.get('/chefs',(req,res)=>{
+      res.send(chefs)
+})
+
 app.listen(port,()=>{
       console.log(`Quisinne De French running on port ${port}`);
 })
