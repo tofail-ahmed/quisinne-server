fe5388ca68b31f01c0812c33333b73b8e5dc3252
Tofail Ahmed Sayem <atofail50@gmail.com>
2023-05-02 22:43:11 +0600

    single chef path created

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express=require('express')
 const app=express()
 const port=5000
+const cors = require('cors')
 const chefs=require("./data/chef.json")
+app.use(cors())
 app.get('/',(req,res)=>{
       res.send("welcome to Quisinne De French")
 })
 app.get('/chefs',(req,res)=>{
       res.send(chefs)
 })
+app.get('/chefs/:id', (req, res) => {
+      const id = req.params.id
+      console.log(id);
+      if (id == 0) {
+            res.send(chefs)
+      } else {
 
+            const selectedChef = chefs.find(chef => chef.chef_id == id)
+            res.send(selectedChef)
+      }
+})
 app.listen(port,()=>{
       console.log(`Quisinne De French running on port ${port}`);
 })
