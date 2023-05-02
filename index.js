246b0bcb904293334b1f66e74d0607ffbe33572f
Tofail Ahmed Sayem <atofail50@gmail.com>
2023-05-02 23:33:04 +0600

    cors installed

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express=require('express')
 const app=express()
 const port=5000
-const cors = require('cors')
+const cors = require('cors');
 const chefs=require("./data/chef.json")
-app.use(cors())
+app.use(cors());
 app.get('/',(req,res)=>{
       res.send("welcome to Quisinne De French")
 })
 app.get('/chefs',(req,res)=>{
-      res.send(chefs)
+      res.send(chefs);
 })
 app.get('/chefs/:id', (req, res) => {
       const id = req.params.id
